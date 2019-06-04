2a625538fc4bc4aea26caf079cb9775dc8ae7f85
Kumail Pirzada <kumailpirzada@gmail.com>
2019-06-04 23:35:28 +0500

    Increased accuracy

--- tweet_topic_classifier.py
+++ tweet_topic_classifier.py
@@ -19,7 +19,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import decomposition, ensemble
 
 # Load spreadsheet with tweets of all users
-df = pd.read_excel('Twitter_timeline.xlsx', sheetname=None, ignore_index=True, sort=True)
+df = pd.read_excel('Twitter_timeline.xlsx', sheet_name=None, ignore_index=True, sort=True)
 cdf = pd.concat(df.values(), ignore_index=True, sort=False)
 
 #print("Number of tweets before removing invalid data: {0}".format(cdf.shape[0]))
@@ -30,6 +30,11 @@ cdf["tags"].fillna(cdf[cdf.columns[2]], inplace=True)
 #drop extra tag column
 cdf.drop(cdf.columns[2], axis=1, inplace=True)
 cdf.dropna(inplace=True)
+cdf = cdf[cdf.tags != "RJ"]
+cdf = cdf[cdf.tags != "Rj"]
+cdf = cdf[cdf.tags != "ET"]
+cdf = cdf[cdf.tags != "EH"]
+cdf = cdf[cdf.tags != "RH"]
 
 tweet_text = cdf[['text', 'tags']]
 tweet_text['id'] = tweet_text.index
@@ -50,6 +55,7 @@ count_vect.fit(cdf['text'])
 # transform the training and validation data using count vectorizer object
 xtrain_count =  count_vect.transform(train_x)
 xvalid_count =  count_vect.transform(valid_x)
+#print(xtrain_count[0])
 
 stemmer = SnowballStemmer('english')
 
@@ -69,18 +75,15 @@ cdf['text'] = processed_docs
 
 #print(xtrain_count)
 
-def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
+def train_model(classifier, feature_vector_train, label, feature_vector_valid):
     # fit the training dataset on the classifier
     classifier.fit(feature_vector_train, label)
     
     # predict the labels on validation dataset
     predictions = classifier.predict(feature_vector_valid)
     
-    if is_neural_net:
-        predictions = predictions.argmax(axis=-1)
-    
     return metrics.accuracy_score(predictions, valid_y)
 
 # Naive Bayes on Count Vectors
-accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+accuracy = train_model(naive_bayes.MultinomialNB(alpha=0.1), xtrain_count, train_y, xvalid_count)
 print ("NB, Count Vectors: ", accuracy)
