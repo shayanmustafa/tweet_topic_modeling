5af4405fef46d0854e6cbfde65f6348afacb32e3
Shayan Mustafa <mustafa.shayan9@gmail.com>
2019-06-04 05:08:37 +0500

    twitter data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -12,11 +12,39 @@ from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
-import numpy as np
 import nltk
 
 # Load spreadsheet with tweets of all users
 df = pd.read_excel('Twitter_timeline.xlsx', sheetname=None, ignore_index=True, sort=True)
 cdf = pd.concat(df.values(), ignore_index=True, sort=False)
-cdf.drop(cdf.columns[5],axis=1)
-print(list(cdf))
+
+print("Number of tweets before removing invalid data: {0}".format(cdf.shape[0]))
+#drop unnecessary attributes
+cdf.drop(["id", "source", "created_at"], axis=1, inplace=True)
+#fill null columns of "tags"
+cdf["tags"].fillna(cdf[cdf.columns[2]], inplace=True)
+#drop extra tag column
+cdf.drop(cdf.columns[2], axis=1, inplace=True)
+cdf.dropna(inplace=True)
+
+tweet_text = cdf[['text']]
+tweet_text['id'] = tweet_text.index
+documents = tweet_text
+#documents = documents.dropna(subset=['text'])
+
+
+stemmer = SnowballStemmer('english')
+
+def lemmatize_stemming(text_to_preprocess):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text_to_preprocess, pos='v'))
+
+def preprocess(text_to_preprocess):
+    result = []
+    for token in gensim.utils.simple_preprocess(text_to_preprocess):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
+
+
+processed_docs = documents['text'].map(preprocess)
+print(processed_docs[:5])
